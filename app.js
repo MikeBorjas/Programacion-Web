8f823812f8d1ab9c9eb17c9e95855c5905522d9d
MikeBorjas <mborjas@ucol.mx>
2020-03-22 20:31:19 -0600

    final

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-var express = require("express"),
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose');
+var express = require("express");
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
 
-var router = express.Router();
-
-router.get('/', function(req, res) {
-   res.send("Hello World!");
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
-app.use(router);
-
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+app.get("/",(req, res, next) =>{
+    
+    res.json(["Prueba","Warzone","Minecraft","Ginger","Food"]);
 });
